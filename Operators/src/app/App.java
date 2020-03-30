c91dffb3d16e55571ec7137f08d309fccd4d8c7a
Nick Winkley <nwinkley@gmail.com>
2020-03-30 13:56:51 -0700

    more Operators work

--- Operators/src/app/App.java
+++ Operators/src/app/App.java
@@ -1,4 +1,7 @@
+// basic operators and intro to if statements, ternary ops
 package app;
+
+import sun.nio.cs.ext.JISAutoDetect;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -24,5 +27,42 @@ public class App {
         if (isAlien == false) {
             System.out.println("It is not an alien!");
         }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score");
+        }
+
+        // AND operator &&
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        // OR operator ||
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        // assignment vs equals to operators
+        int newValue = 50;
+        if(newValue == 50) {
+            System.out.println("This is true");
+        }
+        // shortcut to see if bool is true, "!isCar" tests if false
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        // the ternary operator is a shortcut to assigning one of two values to a variable depending on a given condition
+        // ternary operator is icCar true, then set wasCar to true, if isCar false, set wasCar to false
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
+        
     }
 }
