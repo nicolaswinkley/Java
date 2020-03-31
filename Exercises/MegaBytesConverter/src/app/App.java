6c9ac175a334856c2b51783d518329e00daed157
Nick Winkley <nwinkley@gmail.com>
2020-03-31 14:06:52 -0700

    exercises commit

---
+++ Exercises/MegaBytesConverter/src/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megaBytes = kiloBytes / 1024;
+            int remainderKiloBytes = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainderKiloBytes + " KB");
+        }
+    }
+}
