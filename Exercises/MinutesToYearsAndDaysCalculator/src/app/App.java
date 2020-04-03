447086a8c46655ccfd0e7315f748c85affadf850
Nick Winkley <nwinkley@gmail.com>
2020-04-02 18:55:43 -0700

    MinutesToYearsAndDaysCalculator fixes

--- Exercises/MinutesToYearsAndDaysCalculator/src/app/App.java
+++ Exercises/MinutesToYearsAndDaysCalculator/src/app/App.java
@@ -5,14 +5,14 @@ public class App {
         printYearsAndDays(1051200);
     }
     public static void printYearsAndDays(long minutes) {
-            if (minutes < 0) {
-                System.out.println("Invalid value");
-            }
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
             long hours = minutes / 60;
             long days = hours / 24;
             long years = days / 365;
             long remainingDays = days % 365;
             System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
-
         }
+    }
 }
