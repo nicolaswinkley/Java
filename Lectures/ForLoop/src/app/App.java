6f7b53ffeed5e9b7c5fa8575291986cff37bbe11
Nick Winkley <nwinkley@gmail.com>
2020-04-16 13:49:23 -0700

    prime number test

--- Lectures/ForLoop/src/app/App.java
+++ Lectures/ForLoop/src/app/App.java
@@ -9,10 +9,37 @@ public class App {
 
         }
 
-        
+        int count = 0;
+        for (int i = 10; i < 50; i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println(i + " is prime number");
+                if(count == 3) {
+                    System.out.println("exiting loop");
+                    break;
+                }
+            }
+        }
     }
+    // Create a for statement using any range of numbers
+    // Determine if the number is a prime number using the isPrime method
+    // if it is a prime number, print it out AND increment a count of the
+    // number of prime numbers found
+    // if that count is 3 exit the for loop
+    // hint:  Use the break; statement to exit
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }    
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     public  static double calculateInterest(double amount, double interestRate) {
         return (amount *(interestRate/100));
     }
-    
 }
